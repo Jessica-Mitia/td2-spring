6f7166dc9d9d99963904354a9be21d8c1d8e3745
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-17 21:10:17 +0300

    feat: add the class td2SpringApplication

--- src/main/java/org/example/td2spring/Td2SpringApplication.java
+++ src/main/java/org/example/td2spring/Td2SpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Td2SpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Td2SpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Td2SpringApplication.class, args);
+    }
 
 }
