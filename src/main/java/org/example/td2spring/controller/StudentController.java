7c5a086c13defc652cec18bd27b15724bb3c2bdf
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-17 21:08:29 +0300

    feat: add StudentController

--- src/main/java/org/example/td2spring/controller/StudentController.java
+++ src/main/java/org/example/td2spring/controller/StudentController.java
@@ -1,4 +1,31 @@
 package org.example.td2spring.controller;
 
+import org.example.td2spring.entity.Student;
+import org.example.td2spring.service.StudentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/students")
+    public List<Student> addStudents(@RequestBody List<Student> students) {
+        studentService.saveStudents(students);
+
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
 }
